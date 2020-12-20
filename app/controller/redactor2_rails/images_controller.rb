52df26ac1d70ba02d872658dd05f8ccfe650f0d4
feipinghuang <feiping@guanyun.cn>
2020-12-20 16:15:46 +0800

    Update images_controller.rb

--- app/controller/redactor2_rails/images_controller.rb
+++ app/controller/redactor2_rails/images_controller.rb
@@ -3,8 +3,9 @@ class Redactor2Rails::ImagesController < ApplicationController
   before_action :redactor2_authenticate_user!
 
   def create
-    if params[:file].is_a?(Array)
-      image_files = params[:file].map do |file|
+    file_param = params[:file] || params[:upload]
+    if file_param.is_a?(Array)
+      image_files = file_param.map do |file|
         { data: Redactor2Rails::Http.normalize_param(file, request) }
       end
       images = Redactor2Rails.image_model.create(image_files)
@@ -13,7 +14,7 @@ class Redactor2Rails::ImagesController < ApplicationController
       end
       render json: response
     else
-      image = Redactor2Rails.image_model.create(data: Redactor2Rails::Http.normalize_param(params[:file], request))
+      image = Redactor2Rails.image_model.create(data: Redactor2Rails::Http.normalize_param(file_param, request))
       render json: { id: image.id, url: image.url }
     end
   end
